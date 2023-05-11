585b79460e4b1ac997f292812ad1d6024942d4db
4jur <4jur@proton.me>
2023-05-10 21:12:09 -0400

    Initial demo of Python h2 server and client.

---
+++ mutpatch.py
@@ -0,0 +1,9 @@
+### BEGIN PATCH ###
+# This fixes a problem caused by importing deprecated APIs.
+import collections.abc
+collections.Iterable = collections.abc.Iterable
+collections.Mapping = collections.abc.Mapping
+collections.MutableSet = collections.abc.MutableSet
+collections.MutableMapping = collections.abc.MutableMapping
+### END PATCH ###
+
